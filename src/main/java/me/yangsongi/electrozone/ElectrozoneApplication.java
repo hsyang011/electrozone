72a5110ddbeb04455af6d496280c5fada9ef7435
hsyang011 <hsyang011@gmail.com>
2024-12-14 08:57:51 +0900

    init commit

---
+++ src/main/java/me/yangsongi/electrozone/ElectrozoneApplication.java
@@ -0,0 +1,13 @@
+package me.yangsongi.electrozone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrozoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrozoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yangsongi/electrozone/ElectrozoneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yangsongi.electrozone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrozoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
