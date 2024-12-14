22942bcea195699838cbb3dc36c1c7305bd982db
hsyang011 <hsyang011@gmail.com>
2024-12-15 00:18:58 +0900

    feat(security): integrate Spring Security with UserDetails implementation
    - 스프링 시큐리티 의존성 추가
    - User 엔티티에 UserDetails 인터페이스 상속 구현
    - UserDetailsService 구현하여 사용자 인증 처리

--- src/main/java/me/yangsongi/electrozone/domain/User.java
+++ src/main/java/me/yangsongi/electrozone/domain/User.java
@@ -1,21 +1,23 @@
 package me.yangsongi.electrozone.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +25,7 @@ public class User {
     private Long userId;  // 사용자 고유 ID입니다.
 
     @Column(name = "name", nullable = false)
-    private String name;  // 사용자 이름입니다.
+    private String nickname;  // 사용자 이름입니다.
 
     @Column(name = "email", nullable = false)
     private String email;  // 사용자 이메일입니다.
@@ -44,5 +46,66 @@ public class User {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;  // 사용자 수정 시간입니다.
+
+    @Builder
+    public User(String email, String nickname, String password, String phone) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    // 권한 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    // 사용자의 id를 반환 (고유한 값)
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 사용자의 패스워드 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠금되었는지 확인하는 로직
+        return true; // true -> 잠금되지 않았음
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 패스워드가 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        // 계정이 사용 가능한지 확인하는 로직
+        return true; // true -> 사용 가능
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
 
 }
--- src/main/java/me/yangsongi/electrozone/repository/UserRepository.java
+++ src/main/java/me/yangsongi/electrozone/repository/UserRepository.java
@@ -3,5 +3,10 @@ package me.yangsongi.electrozone.repository;
 import me.yangsongi.electrozone.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email); // email로 사용자 정보를 가져옵니다.
+
 }
