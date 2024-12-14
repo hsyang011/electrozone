d2ae26fa765e06f0f7280963b27ed263fbb01864
hsyang011 <hsyang011@gmail.com>
2024-12-15 02:09:23 +0900

    feat: WebSecurityConfig 설정 완료
    - Spring Security를 통한 인증 및 인가 설정 구현
    - 로그인, 로그아웃, CSRF 비활성화 설정 추가
    - 정적 리소스(css, js 등) 접근 허용 처리

---
+++ src/main/java/me/yangsongi/electrozone/security/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package me.yangsongi.electrozone.security;
+
+import lombok.RequiredArgsConstructor;
+import me.yangsongi.electrozone.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailService userService;
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth // 인증, 인가 설정
+                        .requestMatchers("/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "login", "/signup").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin // 폼 기반 로그인 설정
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/"))
+                .logout(logout -> logout // 로그아웃 설정
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    // 인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService); // 사용자 정보 서비스 설정
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return new ProviderManager(authProvider);
+    }
+
+    // 패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
