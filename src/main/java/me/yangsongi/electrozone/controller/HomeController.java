d506a34bee92bf51df2d5060073ec94954ae2dbf
hsyang011 <hsyang011@gmail.com>
2024-12-15 19:11:19 +0900

    refactor: style.css 파일 이름을 common.css로 변경

--- src/main/java/me/yangsongi/electrozone/controller/HomeController.java
+++ src/main/java/me/yangsongi/electrozone/controller/HomeController.java
@@ -1,6 +1,5 @@
 package me.yangsongi.electrozone.controller;
 
-import me.yangsongi.electrozone.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
