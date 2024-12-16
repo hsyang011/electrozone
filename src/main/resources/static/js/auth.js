4c5b24b5a6f99f4e8b6cbaa435f0365dd484f5eb
hsyang011 <hsyang011@gmail.com>
2024-12-16 10:03:19 +0900

    refactor: 로그인 기능 리팩토링 - 비동기 통신을 통한 폼 데이터 전송 - 상태 코드에 따른 로그인 성공/실패 처리

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -6,13 +6,15 @@ function login(event) {
         method: 'POST',
         body: new FormData(form), // 폼 데이터를 보내는 방식
     }).then(response => {
-        if (response.ok) {
+        if (response.status === 200) { // 로그인 성공 상태 코드 확인
             alert('로그인에 성공하였습니다!');
             location.href = '/';
+        } else if (response.status === 401) {
+            alert('로그인에 실패하였습니다. 아이디나 비밀번호를 확인해주세요.');
         } else {
-            alert('로그인에 실패하였습니다.');
+            alert('서버 오류가 발생했습니다. 나중에 다시 시도해주세요.');
         }
     }).catch(error => {
-       alert(error.message);
+        alert('네트워크 오류: ' + error.message);  // 네트워크 오류에 대한 메시지 처리
     });
 }
