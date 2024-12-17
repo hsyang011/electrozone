eac5d687c2392c16b3309edd8d1f2d28f25df507
hsyang011 <hsyang011@gmail.com>
2024-12-17 09:56:49 +0900

    feat: dto 클래스를 레코드로 변경하고, getter를 쓰는 부분을 레코드 문법에 맞게 수정

--- src/main/java/me/yangsongi/electrozone/dto/CreateAccessTokenResponse.java
+++ src/main/java/me/yangsongi/electrozone/dto/CreateAccessTokenResponse.java
@@ -1,12 +1,5 @@
 package me.yangsongi.electrozone.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class CreateAccessTokenResponse {
-
-    private String accessToken;
+public record CreateAccessTokenResponse(String accessToken) {
 
 }
