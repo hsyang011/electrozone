62459a78f8f12288f03a51187d6d342f4354367f
hsyang011 <hsyang011@gmail.com>
2024-12-18 16:45:10 +0900

    refactor: 리프레시 토큰과 관련된 메소드를 RefreshTokenService로 이동

--- src/main/java/me/yangsongi/electrozone/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/me/yangsongi/electrozone/config/oauth/OAuth2SuccessHandler.java
@@ -1,15 +1,12 @@
 package me.yangsongi.electrozone.config.oauth;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.yangsongi.electrozone.config.jwt.TokenProvider;
-import me.yangsongi.electrozone.domain.RefreshToken;
 import me.yangsongi.electrozone.domain.User;
-import me.yangsongi.electrozone.repository.RefreshTokenRepository;
+import me.yangsongi.electrozone.service.RefreshTokenService;
 import me.yangsongi.electrozone.service.UserService;
-import me.yangsongi.electrozone.util.CookieUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -23,14 +20,13 @@ import java.time.Duration;
 @Component
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
     public static final String REDIRECT_PATH = "/";
 
     private final TokenProvider tokenProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
     private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+    private final RefreshTokenService refreshTokenService;
     private final UserService userService;
 
     @Override
@@ -40,37 +36,16 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         // 리프레시 토큰 생성 -> 저장 -> 쿠키에 저장
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
-        saveRefreshToken(user.getUserId(), refreshToken);
-        addRefreshTokenToCookie(request, response, refreshToken);
+        refreshTokenService.saveRefreshToken(user.getUserId(), refreshToken);
+        refreshTokenService.addRefreshTokenToCookie(request, response, refreshToken);
         // 액세스 토큰 생성 -> 패스에 액세스 토큰 추가
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
         String targetUrl = getTargetUrl(accessToken);
         // 인증 관련 설정값, 쿠키 제거
-        clearAuthenticationAttributes(request, response);
-        // 리다이렉트
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
-    }
-
-    // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장
-    private void saveRefreshToken(Long userId, String newRefreshToken) {
-        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
-                .map(entity -> entity.update(newRefreshToken))
-                .orElse(new RefreshToken(userId, newRefreshToken));
-
-        refreshTokenRepository.save(refreshToken);
-    }
-
-    // 생성된 리프레시 토큰을 쿠키에 저장
-    private void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
-        int cookieMaxAge = (int)REFRESH_TOKEN_DURATION.toSeconds();
-        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
-        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
-    }
-
-    // 인증 관련 설정값, 쿠키 제거
-    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
         super.clearAuthenticationAttributes(request);
         authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+        // 리다이렉트
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
     // 액세스 토큰을 패스에 추가
--- src/main/java/me/yangsongi/electrozone/service/RefreshTokenService.java
+++ src/main/java/me/yangsongi/electrozone/service/RefreshTokenService.java
@@ -1,19 +1,43 @@
 package me.yangsongi.electrozone.service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.yangsongi.electrozone.domain.RefreshToken;
 import me.yangsongi.electrozone.repository.RefreshTokenRepository;
+import me.yangsongi.electrozone.util.CookieUtil;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
 
     private final RefreshTokenRepository refreshTokenRepository;
 
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+    // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장
+    public void saveRefreshToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
+    }
+
+    // 생성된 리프레시 토큰을 쿠키에 저장
+    public void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+        int cookieMaxAge = (int)REFRESH_TOKEN_DURATION.toSeconds();
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
     }
 
 }
