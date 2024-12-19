5d54b3a73a6398a8c75244f3236a934b739ba4ee
hsyang011 <hsyang011@gmail.com>
2024-12-19 21:42:45 +0900

    refactor: 소소한 기능 수정 - OAuth2 로그인 시 URL에 액세스 토큰이 남아있는 문제가 발생 => 한 번 더 /로 리다이렉트하여 해결 - 회원찾기 페이지에서 불필요한 로그인/회원가입 링크 제거

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -3,6 +3,7 @@ const oauth2token = new URLSearchParams(location.search).get('token');
 if (oauth2token) {
     localStorage.setItem('access_token', oauth2token);
     alert('로그인에 성공하였습니다!');
+    location.href = '/';
 }
 
 function login(event) {
