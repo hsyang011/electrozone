d4d988bc261a58da915558e9c5bba50e6cc5471b
hsyang011 <hsyang011@gmail.com>
2024-12-21 10:57:35 +0900

    feat: index.html에 상품을 리스트하는 뷰 컨트롤러 추가

--- src/main/java/me/yangsongi/electrozone/repository/ProductRepository.java
+++ src/main/java/me/yangsongi/electrozone/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package me.yangsongi.electrozone.repository;
 import me.yangsongi.electrozone.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findTop6ByOrderByRegisteredAtDesc();
+
 }
